591d1e1257834a7ff71cef420d8146b5fd885d94
pawanpdn-671 <pawanpradhanpc671@gmail.com>
2021-09-24 23:47:00 +0530

    final commit

---
+++ assets/js/ads.js
@@ -0,0 +1,28 @@
+var intervalID;
+var closeBtn = document.querySelector('#closeAd');
+var adBox = document.querySelector('.ad');
+
+
+window.addEventListener('load', () => {
+    intervalID = setInterval(async () => {
+        
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://source.unsplash.com/random/500x500', true);
+        xhr.onload = function() {
+            if(this.status == 200){
+                let response = xhr.responseURL;
+                adBox.innerHTML = `<img src=${response} />`;
+                closeBtn.style.display="inline-block";
+            }
+        }
+        xhr.send();
+    }, 5000);
+    
+})
+
+
+
+closeBtn.addEventListener('click', () => {
+    clearInterval(intervalID);
+    document.querySelector('.ad-block').style.display='none';
+});
